539595ff8d8966b4404c898cc37335777f86d663
Serhii <drimding@gmail.com>
2021-05-23 00:12:40 +0300

    "fix mysql travis"

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BASE_DIR = pathlib.Path(__file__).parent
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:@127.0.0.1:3306/develop'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:root@127.0.0.1:3306/develop'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY') or 'secret-key-goes-here'
 
